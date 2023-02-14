87061a424957b5d64b836640f222d370ff875c0f
ChristianRichardt <christian.richardt@utb.helsingborg.se>
2023-02-14 12:29:19 +0100

    Skapelsen av kontakt klassen

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,4 @@
 fun main(args: Array<String>) {
-    println("Skriv ditt namn:")
-    val name = readln()
-    println("Hello $name")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val person = Kontakt(firstName = "Christian", lastName = "Richardt", phoneNumber = "0762094322", eMail = "christian@gmail.com" )
+    println(person.fName + " " + person.lName + " " + person.pNumber + " " + person.mail);
 }
