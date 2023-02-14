0f41271a6964c1070354a1f81c3cc3c280593f90
ChristianRichardt <christian.richardt@utb.helsingborg.se>
2023-02-14 12:35:59 +0100

    Variabler i kontakt klassen

--- src/main/kotlin/Kontakt.kt
+++ src/main/kotlin/Kontakt.kt
@@ -1,3 +1,6 @@
-class Kontakt {
-
+class Kontakt(firstName:String, lastName:String, phoneNumber:String, eMail:String) {
+    var fName = firstName;
+    var lName = lastName;
+    var pNumber = phoneNumber;
+    var mail = eMail;
 }
