32ca0b2979998499f60aeb96d5c395a656e2b670
ChristianRichardt <christian.richardt@utb.helsingborg.se>
2023-02-14 13:18:12 +0100

    Till Mastern

--- src/main/kotlin/Kontakt.kt
+++ src/main/kotlin/Kontakt.kt
@@ -1,4 +1,4 @@
-class Kontakt(firstName:String, lastName:String, phoneNumber:String, eMail:String) {
+class Kontakt(firstName:String, lastName:String, phoneNumber:String, eMail:String) { // Variable deklarering i Kontakt
     var fName = firstName;
     var lName = lastName;
     var pNumber = phoneNumber;
