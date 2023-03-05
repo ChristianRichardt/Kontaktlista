acb4fc9ea12264ae9b07233bb5adf8b4f23a3a95
ChristianRichardt <christian.richardt@utb.helsingborg.se>
2023-03-05 22:03:45 +0100

    Meny

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,42 +1,43 @@
-//import kotlin.contracts.contract
-
 fun main()
 {
-    menu()
-}
-
-fun menu() {
     val fileSystem = File()
     val telefonbok = fileSystem.readFiles()
 
-    if (telefonbok != null) {
+    if(telefonbok != null){
+        while(true){
+            println("[1] Vissa alla kontakter")
+            println("[2] Lägg till en kontakt")
+            println("[3] Ta bort en kontakt")
+            println("[4] Ändra en kontakts information \n")
+            println("[0] Stäng programmet")
 
+            val choice = readln().toInt()
 
-        while (true) {
-            println("\n------------------------------------------\n")
-            println("[1] Lägg till kontakt")
-            println("[2] Ta bort kontakt")
-            println("[3] Redigera en kontakt")
-            println("[4] Visa alla kontakter")
-            println("\n------------------------------------------\n")
-            val menuInput = readln()
-
-            if (menuInput == "1") {
-                telefonbok.createKontaktObject()
-            } else if (menuInput == "2") {
-                telefonbok.deleteAContact()
-            } else if (menuInput == "3") {
-                telefonbok.editAContact()
-            } else if (menuInput == "4") {
+            if(choice == 1){
                 telefonbok.displayAllContacts()
+                readln()
+                continue
             }
-
-            println("\n------------------------------------------\n")
-
-            readlnOrNull()
-
+            else if(choice == 2){
+                telefonbok.createKontaktObject()
+                readln()
+                continue
+            }
+            else if(choice == 3){
+                telefonbok.deleteAContact()
+                readln()
+                continue
+            }
+            else if(choice == 4){
+                telefonbok.editAContact()
+                readln()
+                continue
+            }
+            else if(choice == 0){
+                break
+            }
         }
-
     }
-}
 
+
+}
